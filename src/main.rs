13c3192e4b19788688eba15d8c0f584f53941eb9
IgooorGP <igor.feq@gmail.com>
2021-06-07 21:11:21 -0300

    feat: refactor main entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,25 @@
 mod entities;
 mod pong;
 mod sprite_sheet;
+mod startup;
 mod systems;
 
-use amethyst::{
-    core::TransformBundle,
-    input::{InputBundle, StringBindings},
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    utils::application_root_dir,
-};
+use amethyst::{prelude::*, utils::application_root_dir};
+use startup::{build_game_config, setup_logger};
 
 fn main() -> amethyst::Result<()> {
-    // logger setup
-    amethyst::start_logger(Default::default());
-
-    // paths
     let app_root = application_root_dir()?;
     let assets_dir = app_root.join("assets");
 
-    let display_config_path = app_root.join("config").join("display.ron");
-    let key_bindings_path = app_root.join("config").join("key_bindings.ron");
+    // game config and game instance building
+    let game_config = build_game_config(&app_root)?;
+    let mut game = Application::new(assets_dir, pong::Pong::default(), game_config)?;
 
-    // input handler: parameter type determines how the axes/actions are read -> here strings are used!
-    let input_bundle =
-        InputBundle::<StringBindings>::new().with_bindings_from_file(key_bindings_path)?;
+    // logger setup
+    setup_logger();
 
-    // data builder contains all the ECS bundles and systems used to configure the game
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.0, 0.0, 0.0, 1.0]),
-                )
-                .with_plugin(RenderFlat2D::default()),
-        )?
-        .with_bundle(TransformBundle::new())? // bundle for tracking entities positions
-        .with_bundle(input_bundle)? // bundle for reading inputs
-        .with(systems::PaddleSystem, "paddle_system", &["input_system"]) // system (not bundle) -> input sys must run b4!
-        .with(systems::BallSystem, "ball_system", &[])
-        .with(
-            systems::CollisionSystem,
-            "collision_system",
-            &["paddle_system", "ball_system"],
-        );
-
-    // builds the game application -> pong::Pong contains the state and game hooks (on_start, etc.)
-    let mut game = Application::new(assets_dir, pong::Pong::default(), game_data)?;
-
-    // main game loop
+    // run game loop
     game.run();
 
-    // final status code
     Ok(())
 }
---
+++ src/startup.rs
@@ -0,0 +1,51 @@
+use std::path::PathBuf;
+
+use amethyst::{
+    core::TransformBundle,
+    input::{InputBundle, StringBindings},
+    renderer::{types::DefaultBackend, RenderFlat2D, RenderToWindow, RenderingBundle},
+    start_logger, GameDataBuilder,
+};
+
+use crate::systems;
+
+/// Builds the game configuration by pluging bundles and systems to it.
+pub fn build_game_config(
+    app_root: &PathBuf,
+) -> amethyst::Result<GameDataBuilder<'static, 'static>> {
+    // paths based on app root path
+    let display_config_path = app_root.join("config").join("display.ron");
+    let key_bindings_path = app_root.join("config").join("key_bindings.ron");
+
+    // input handler: parameter type determines how the axes/actions are read
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(key_bindings_path)?;
+
+    // game configuration building: use bundles for rendering, for transforms, inputs, plug systems, etc.
+    let game_config = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default()),
+        )?
+        .with_bundle(TransformBundle::new())? // bundle for tracking entities positions
+        .with_bundle(input_bundle)? // bundle for reading inputs
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::BallSystem, "ball_system", &[])
+        .with(
+            systems::CollisionSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        );
+
+    Ok(game_config)
+}
+
+/// Configures Amethyst's logger.
+pub fn setup_logger() {
+    // builds loger with default values
+    start_logger(Default::default());
+}
